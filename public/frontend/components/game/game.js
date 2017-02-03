584bdad00d12c7a153730a84433b355df0308c39
marci <marci@chrome.hu>
2017-02-03 18:33:21 +0100

    cleanup/refactor

--- public/frontend/components/game/game.js
+++ public/frontend/components/game/game.js
@@ -9,7 +9,7 @@ angular.module('GameOfLife.game', ['ngRoute'])
         });
     }])
 
-    .controller('gameCtrl', ['$scope', '$http', '$location', '$interval', function ($scope, $http, $location, $interval) {
+    .controller('gameCtrl', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
         var self = this;
         self.cells = [
             {x: 0, y: 1},
@@ -22,14 +22,16 @@ angular.module('GameOfLife.game', ['ngRoute'])
         self.displayYmin = -15;
         self.displayXmax = 15;
         self.displayYmax = 15;
-        var table = [];
         self.offsetX = 0;
         self.offsetY = 0;
+        self.loadables = [];
+        self.run = false;
+        self.interval = 1000;
         var draw = function () {
-          self.table = self.getTable(self.cells);
+            self.table = self.getTable(self.cells);
         };
         self.evolve = function () {
-            $http.post('/evolve',self.cells
+            $http.post('/evolve', self.cells
             ).then(function successCallback(response) {
                 self.cells = response.data.result;
                 draw();
@@ -41,58 +43,51 @@ angular.module('GameOfLife.game', ['ngRoute'])
         self.toggleCell = function (x, y) {
             var found = false;
             self.cells.forEach(function (cell, idx) {
-                if (cell.x == x && cell.y == y){
+                if (cell.x == x && cell.y == y) {
                     var tempCells = self.cells;
                     self.cells = [];
                     tempCells.forEach(function (cell, idx2) {
-                      if (idx2 == idx) return;
-                      self.cells.push(cell);
+                        if (idx2 == idx) return;
+                        self.cells.push(cell);
                     });
                     found = true;
                 }
             });
-            if (!found){
-                self.cells.push({x:x, y:y, state:1});
+            if (!found) {
+                self.cells.push({x: x, y: y, state: 1});
             }
             draw();
         };
 
-        self.getHeader = function () {
-            var indecies = [];
-            var t = self.table.slice();
-            var l = t.pop();
-          for(var i = 0; i < l.length; i++){
-              indecies.push(i- self.offsetY);
-          }
-          return indecies;
-        };
-
         self.getTable = function (cells) {
-            if (self.displayXmin < 0){
+            //we're offsetting positions, because we cant have negative indexes in js
+            if (self.displayXmin < 0) {
                 self.offsetX = -self.displayXmin;
             }
-            if (self.displayYmin < 0){
+            if (self.displayYmin < 0) {
                 self.offsetY = -self.displayYmin;
             }
             var tableForm = [];
+            //initialitzing empty board
             for (var i = self.displayXmin + self.offsetX; i <= self.displayXmax + self.offsetX; i++) {
                 tableForm[i] = [];
                 for (var j = self.displayYmin + self.offsetY; j <= self.displayYmax + self.offsetY; j++) {
-                    tableForm[i][j] = {state: 0, originalX: i-self.offsetX, originalY: j-self.offsetY};
+                    tableForm[i][j] = {state: 0, originalX: i - self.offsetX, originalY: j - self.offsetY};
                 }
             }
+            //putting in the live cells
             cells.forEach(function (cell) {
-                if(cell.x <= self.displayXmax && cell.y <= self.displayYmax && cell.x+ self.offsetX >= self.displayXmin && cell.y + self.offsetY>= self.displayYmin){
-                    tableForm[cell.x + self.offsetX][cell.y + self.offsetY] = {state: 1, originalX: cell.x, originalY: cell.y};
+                if (cell.x <= self.displayXmax && cell.y <= self.displayYmax && cell.x + self.offsetX >= self.displayXmin && cell.y + self.offsetY >= self.displayYmin) {
+                    tableForm[cell.x + self.offsetX][cell.y + self.offsetY] = {
+                        state: 1,
+                        originalX: cell.x,
+                        originalY: cell.y
+                    };
                 }
             });
             return tableForm;
 
         };
-        self.table = self.getTable(self.cells);
-
-        self.run = false;
-        self.interval = 1000;
         var poller = null;
         var start = function () {
             self.run = true;
@@ -107,24 +102,12 @@ angular.module('GameOfLife.game', ['ngRoute'])
         };
 
         self.toggleRun = function () {
-            if (self.run){
+            if (self.run) {
                 stop();
             } else {
                 start();
             }
         };
-
-        self.loadables = [];
-        $http({
-            method: 'GET',
-            url: '/list'
-        }).then(function successCallback(response) {
-            self.loadables = response.data;
-            self.selectedFile = self.loadables[0];
-        }, function errorCallback(response) {
-
-        });
-
         self.load = function () {
             $http({
                 method: 'GET',
@@ -140,6 +123,29 @@ angular.module('GameOfLife.game', ['ngRoute'])
         };
         $scope.$on("$destroy", function handler() {
             stop();
+        });
+
+        self.getHeader = function () {
+            var indecies = [];
+            var t = self.table.slice();
+            var l = t.pop();
+            for (var i = 0; i < l.length; i++) {
+                indecies.push(i - self.offsetY);
+            }
+            return indecies;
+        };
+
+
+        //init
+        self.table = self.getTable(self.cells);
+        $http({
+            method: 'GET',
+            url: '/list'
+        }).then(function successCallback(response) {
+            self.loadables = response.data;
+            self.selectedFile = self.loadables[0];
+        }, function errorCallback(response) {
+
         });
 
 
